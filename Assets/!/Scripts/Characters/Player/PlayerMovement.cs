99d9ac5dd008ad2f860499bfcd34fc5b6064156b
IchiSamaFR <djordan-dsousa@hotmail.fr>
2023-02-03 12:19:01 +0100

    Add - Tiles

--- Assets/!/Scripts/Characters/Player/PlayerMovement.cs
+++ Assets/!/Scripts/Characters/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ namespace Gladiator.Character.Player
 {
     public class PlayerMovement : CharacterMovement
     {
+        public Transform Transform;
         private Player player;
 
         public Player Player
@@ -23,9 +24,15 @@ namespace Gladiator.Character.Player
 
         private void FixedUpdate()
         {
+            print(AngleBetweenVector2(transform.position, Transform.position));
             Movement();
         }
-
+        private float AngleBetweenVector2(Vector2 vec1, Vector2 vec2)
+        {
+            Vector2 diference = vec2 - vec1;
+            float sign = (vec2.y < vec1.y) ? -1.0f : 1.0f;
+            return Vector2.Angle(Vector2.right, diference) * sign;
+        }
         public void Movement()
         {
             if (!Player.PlayerStats.CanMove)
@@ -51,10 +58,10 @@ namespace Gladiator.Character.Player
                 direction -= Vector3.left;
             }
 
-            if(Input.GetKey(KeyCode.Space)) 
+            if (Input.GetKey(KeyCode.Space))
             {
                 Player.PlayerStats.IsDashing = true;
-                Dash(direction,Player.PlayerStats.DashRange, 0.1f, () => Player.PlayerStats.IsDashing = false);
+                Dash(direction, Player.PlayerStats.DashRange, 0.1f, () => Player.PlayerStats.IsDashing = false);
             }
             MoveDirection(direction, Player.PlayerStats.MoveSpeed);
         }
